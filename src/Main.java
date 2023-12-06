ef4bd3aacad014d27a575c5eaf97198ffa051487
MarcosFrMelo <porquenaodeixar@hotmail.com>
2023-12-06 14:25:18 -0300

    Initial Comit for this project

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,6 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        int idade;
         System.out.println("Hello! world, i am good!:D");
-        System.out.println("teste, ok?");
-        Scanner idade;
-        System.out.println("Sua idade é" + idade);
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
     }
 }
