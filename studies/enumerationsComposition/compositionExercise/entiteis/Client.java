1b63d8da304fecf36cfac4f94e554263f2e0bc76
Marcos Freire de Melo <porquenaodeixar@hotmail.com>
2023-12-23 15:10:10 -0300

    customer registration function

--- studies/enumerationsComposition/compositionExercise/entiteis/Client.java
+++ studies/enumerationsComposition/compositionExercise/entiteis/Client.java
@@ -1,11 +1,20 @@
 package enumerationsComposition.compositionExercise.entiteis;
 
 import java.sql.Time;
+import java.time.LocalDate;
 
 public class Client {
     private String name;
     private String email;
-    private Time birthDate;
+    private LocalDate birthDate;
     private Order order;
 
+    public Client() {
+    }
+
+    public Client(String name, String email, LocalDate birthDate) {
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
 }
--- studies/enumerationsComposition/compositionExercise/program/Program.java
+++ studies/enumerationsComposition/compositionExercise/program/Program.java
@@ -2,25 +2,28 @@ package enumerationsComposition.compositionExercise.program;
 
 import enumerationsComposition.compositionExercise.entiteis.Client;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.*;
 
 public class Program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Client> clientList = new ArrayList<>();
-        Client client = new Client();
 
         do {
-            System.out.println("Enter the option you want\n" +
-                    "1 - Register Customer\n" +
-                    "2 - Register Products\n" +
-                    "3 - Edit a product\n" +
-                    "4 - Delete a product or customer\n" +
-                    "5 - Exit");
+            System.out.println("""
+                    Enter the option you want
+                    1 - Register Customer
+                    2 - Register Products
+                    3 - Edit a product
+                    4 - Delete a product or customer
+                    5 - Exit""");
             int option = scanner.nextInt();
-            switch (option){
+            switch (option) {
                 case 1:
-                    registerCostumer(client, clientList);
+                    registerCostumer(clientList);
                     break;
                 case 2:
                     registerProducts(clientList);
@@ -37,7 +40,7 @@ public class Program {
             }
 
 
-        }while(true);
+        } while (true);
 
     }
 
@@ -50,25 +53,34 @@ public class Program {
     private static void registerProducts(List<Client> clientList) {
     }
 
-    private static void registerCostumer(Client client, List<Client> clientList) {
+    private static void registerCostumer(List<Client> clientList) {
         Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
         System.out.println("Enter the number of customers you will register");
         int clientQuantity = scanner.nextInt();
 
-        for (int i = clientList.size(); i < clientQuantity + (clientQuantity-1); i++){
+        for (int i = clientList.size(); i < clientQuantity + (clientQuantity - 1); i++) {
             scanner.nextLine();
-            System.out.println("");
+            System.out.println("Enter the customer's name");
             String name = scanner.nextLine();
-            System.out.println("");
+            System.out.println("Enter the customer's email");
             String email = scanner.nextLine();
-            System.out.println("");
-
+            boolean tryCatch;
+            do {
+                System.out.println("Enter the customer's date of birth (day/month/year)");
+                String input = scanner.nextLine();
+                try {
+                    LocalDate birthDate = LocalDate.parse(input, formatter);
+                    Client client = new Client(name, email, birthDate);
+                    clientList.add(client);
+                    tryCatch = false;
+                } catch (DateTimeParseException e) {
+                    System.out.println("Date of birth entered is incorrect");
+                    tryCatch = true;
+                }
+            } while (tryCatch);
+            System.out.println("customer successfully registered");
         }
-
-
-
-
-
     }
 }
